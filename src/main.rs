9c85d4687692a4cdf5f88c09a788fb917696e20a
Arthurdw <mail@arthurdw.com>
2020-12-23 11:46:40 +0100

    ✨ Removed CORS since this will be handled by nginx.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate serde_json;
 extern crate tower_web;
 
 use tower_web::ServiceBuilder;
-use tower_web::middleware::cors::{CorsBuilder, AllowedOrigins, CorsMiddleware};
-use std::time::Duration;
 
 mod routes;
 mod types;
@@ -13,15 +11,9 @@ pub fn main() {
     let addr = "127.0.0.1:25580".parse().expect("Port already in use");
     println!("Listening on http://{}", addr);
 
-    let cors = CorsBuilder::new()
-        .allow_origins(AllowedOrigins::Any { allow_null: false })
-        .max_age(Duration::new(300, 0))
-        .build();
-
     ServiceBuilder::new()
         .resource(routes::token::TokenResource)
         .resource(routes::code::CodeResource)
-        .middleware(CorsMiddleware::from(cors))
         .run(&addr)
         .unwrap()
 }
