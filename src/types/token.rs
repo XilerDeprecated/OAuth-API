d3a5b22e437dea7c594bc574d233c2a510fc7127
Arthurdw <mail@arthurdw.com>
2020-12-24 13:50:19 +0100

    ✨ Implemented CORS for the /code route.

--- src/types/token.rs
+++ src/types/token.rs
@@ -21,6 +21,7 @@ pub struct TokenData {
 
 #[derive(Debug, Response)]
 #[web(status = "200")]
+
 pub struct TokenResponse {
     pub status: super::super::types::status::RequestStatus,
     pub data: Option<TokenData>,
