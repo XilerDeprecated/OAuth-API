4f57e5b1aacc9d7a62d5ea96ff420b3006e17c9d
Arthurdw <mail@arthurdw.com>
2020-12-24 16:30:19 +0100

    ❌ Removed last commit contents.

--- src/types/code.rs
+++ src/types/code.rs
@@ -5,11 +5,6 @@ pub struct OAuthCode {
 
 #[derive(Debug, Response)]
 #[web(status = "200")]
-#[web(header(name = "access-control-allow-origin", value = "*"))]
-#[web(header(name = "access-control-allow-methods", value = "GET"))]
-#[web(header(name = "access-control-allow-headers", value = "authorization, cache-control, if-match, if-modified-since, if-none-match, if-unmodified-since, range"))]
-#[web(header(name = "access-control-expose-headers", value = "content-length, content-type"))]
-#[web(header(name = "access-control-allow-credentials", value = "true"))]
 pub struct TokenResponse {
     pub status: super::super::types::status::RequestStatus,
     pub data: Option<OAuthCode>,
