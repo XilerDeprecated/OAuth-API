f5ba408b6e852674756d7b1d172aa1eb82336d73
Arthurdw <mail@arthurdw.com>
2020-12-26 11:15:26 +0100

    🌟✨ Implemented redis Auth for the /token endpoint and improved code quality.

--- src/routes/token.rs
+++ src/routes/token.rs
@@ -1,11 +1,24 @@
-use redis::{Commands, ToRedisArgs, RedisResult};
+use redis::{Commands, RedisResult, ToRedisArgs};
 
 use crate::types::code::OAuthCode;
 use crate::types::status::RequestStatus;
 use crate::types::token::*;
+use crate::utils::env::get_env_var;
 
 #[derive(Clone, Debug)]
 pub struct TokenResource;
+
+fn create_token_response(message: &'static str, code: u16, data: Option<TokenData>) -> Result<TokenResponse, ()> {
+    Ok(TokenResponse {
+        status: RequestStatus { message, code },
+        data,
+    })
+}
+
+fn create_invalid_token_response(message: &'static str, code: u16) -> Result<TokenResponse, ()> {
+    create_token_response(message, code, None)
+}
+
 
 impl_web! {
     impl TokenResource {
@@ -15,57 +28,31 @@ impl_web! {
             match redis::Client::open("redis://127.0.0.1:6379/") {
                 Ok(client) => match client.get_connection() {
                     Ok(mut conn) => {
-                        // let pass = match env::var("REDIS_PASS") {
-                        //     Ok(val) => val,
-                        //     Err(_) => "".to_string(),
-                        // };
-                        // let _: () = redis::cmd("AUTH").arg(pass).query(&mut conn).unwrap();
+                        let pass = get_env_var("REDIS_PASS");
+                        let _: () = redis::cmd("AUTH").arg(pass).query(&mut conn).unwrap();
                         let data: RedisResult<String> = conn.get(body.code.to_redis_args());
                         match data {
                             Ok(user) => {
                                 let _ : () = conn.del(&body.code).unwrap();
-
-                                println!("Fetching data for {}", user);
-
                                 // TODO: FETCH FROM DB
-                                Ok(TokenResponse {
-                                    status: RequestStatus {
-                                        message: "Successfully created a new token",
-                                        code: 0
-                                    },
-                                    data: Some(TokenData {
+                                create_token_response(
+                                    "Successfully created a new token",
+                                    0,
+                                    Some(TokenData {
                                         access_token: "4eabde45ff5626562cf8e9dc7c0abf8f.9c8d9442383b05476310e0e#66c66b94",
                                         app: "83ed1",
                                         refresh_token: "492e50f22d0941c54afea8465fe3813f.72f316244ee6fe236925c36#a2a9ffae",
                                         token_type: 1,
                                         expires_in: 604800,
-                                    }),
-                                })
+                                    })
+                                )
                             },
-                            Err(_) => Ok(TokenResponse {
-                                status: RequestStatus {
-                                    message: "Could not find the requested code",
-                                    code: 400
-                                },
-                                data: None
-                            })
+                            Err(_) => create_invalid_token_response("Could not find the requested code", 400)
                         }
                     },
-                    Err(_) => Ok(TokenResponse {
-                        status: RequestStatus {
-                            message: "Could not create connection with redis database",
-                            code: 500
-                        },
-                        data: None
-                    })
+                    Err(_) => create_invalid_token_response("Could not create connection with redis database", 500)
                 },
-                Err(_) => Ok(TokenResponse {
-                    status: RequestStatus {
-                        message: "Could not create redis client",
-                        code: 500
-                    },
-                    data: None
-                })
+                Err(_) => create_invalid_token_response("Could not create redis client", 500)
             }
         }
     }
--- src/types/code.rs
+++ src/types/code.rs
@@ -6,6 +6,6 @@ pub struct OAuthCode {
 #[derive(Debug, Response)]
 #[web(status = "200")]
 pub struct TokenResponse {
-    pub status: super::super::types::status::RequestStatus,
+    pub status: crate::types::status::RequestStatus,
     pub data: Option<OAuthCode>,
 }
